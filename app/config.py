ec202c519130f2e7aa04634c36739789fa9c2689
tagirmukail <mukailov1991@gmail.com>
2017-12-06 13:13:20 +0300

    update

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ MONGO_HOST = '127.0.0.1'
 MONGO_PORT = '27017'
 MONGO_DBNAME = 'admin'
 MONGO_USERNAME = 'root'
-MONGO_PASSWORD = 'nogiruki'
+MONGO_PASSWORD = 'password'
 MONGO_AUTO_START_REQUEST = 'True'
 
 SECRET_KEY = 'mysecret!'
@@ -14,9 +14,9 @@ TRAP_HTTP_EXCEPTIONS = True
 host = '127.0.0.1'
 port = 5000
 
-MAIL_SERVER = 'smtp.mail.ru'
-MAIL_USERNAME = 'mukailov1991@mail.ru   '
-MAIL_PASSWORD = 'MTagirdag05reg'
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_USERNAME = 'yourmail'
+MAIL_PASSWORD = 'yourpassword'
 MAIL_PORT = 465
 MAIL_USE_SSL = True
 MAIL_USE_TLS = False
