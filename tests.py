13e1860c3309736156e06b23885fbb1bf64726ae
tagirmukail <mukailov1991@gmail.com>
2017-12-08 16:49:45 +0300

    update test url='/recv/'

--- tests.py
+++ tests.py
@@ -1,14 +1,13 @@
-
 from app import app
 import unittest
 from unittest import TestCase
-#from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 
 class TestEhoCase(TestCase):
     def setUp(self):
-        app.config['MONGO_DBNAME'] = 'test'
         self.app = app.test_client()
-
+        app.config['MONGO_DBNAME'] = 'test'
+        app.config['TESTING'] = True
 
     def tearDown(self):
         pass
@@ -24,6 +23,12 @@ class TestEhoCase(TestCase):
     def test_index(self):
         rv = self.app.get('/')
         assert rv.status_code == 302
+
+    def test_recv(self):
+        rv = self.app.get('/recv/')
+        assert rv.status_code == 302
+
+
 
 if __name__ == '__main__':
     unittest.main()
